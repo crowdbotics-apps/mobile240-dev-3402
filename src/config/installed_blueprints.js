eac1ee77a78e6e83c7ba7e8b5bc67c4ce1f780dc
Crowdbotics <team@crowdbotics.com>
2020-04-27 08:14:41 +0000

    add package blueprint-react-native-emailauth-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'EmailAuth3119', human_name: 'Email Auth', access_route: 'EmailAuth3119', icon: 'envelope-o'},
 { name: 'EmailAuth3118', human_name: 'Email Auth', access_route: 'EmailAuth3118', icon: 'envelope-o'},
 { name: 'EmailAuth3117', human_name: 'Email Auth', access_route: 'EmailAuth3117', icon: 'envelope-o'},
 { name: 'EmailAuth3057', human_name: 'Email Auth', access_route: 'EmailAuth3057', icon: 'envelope-o'},
